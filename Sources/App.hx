2b174ce3484cc0512a5f9d9eaa741c9c959195b9
Aksel Kornesjö <Aksel.Kornesjo@explizit.se>
2016-11-11 13:45:49 +0100

    updates

--- Sources/App.hx
+++ Sources/App.hx
@@ -15,6 +15,7 @@ import kek.physics.SpringSystem;
 class App {
 	var pipeline:PipelineState;
 	var springs:SpringSystem;
+	var mouse:kha.input.Mouse;
 
 	public function new() {
 		springs = new SpringSystem();
@@ -29,12 +30,37 @@ class App {
 		pipeline.depthWrite = true;
 		pipeline.depthMode = kha.graphics4.CompareMode.LessEqual;
 		pipeline.compile();
-		var mouse = kha.input.Mouse.get();
-		mouse.notify(mouseDown, null, null, null);
+		mouse = kha.input.Mouse.get();
+		mouse.notify(mouseDown, mouseUp, mouseMove, null);
+	}
+	
+	var friction:Float = 0.9;
+	var mdown = false;
+	var moveX:Float = 0;
+	var moveY:Float = 0;
+	var x:Float = 0;
+	var y:Float = 0;
+	
+	function mouseMove(x:Int, y:Int, dx:Int, dy:Int) {
+		trace('Mouse: $x, $y. Move: $dx, $dy');
+		if(mdown) {
+			moveX += dx;
+			moveY += dy;
+		}
+	}
+	
+	function mouseUp(x:Int, y:Int, i:Int) {
+		trace('Up: $x, $y, $i');
+		kha.SystemImpl.unlockMouse();
+		mouse.showSystemCursor();
+		mdown = false;
 	}
 	
 	function mouseDown(x:Int, y:Int, i:Int) {
-		trace('$x, $y, $i');
+		trace('Down: $x, $y, $i');
+		kha.SystemImpl.lockMouse();
+		mouse.hideSystemCursor();
+		mdown = true;
 	}
 
 	function update(): Void {
@@ -42,10 +68,22 @@ class App {
 	}
 
 	function render(framebuffer: Framebuffer): Void {		
-		var g2 = framebuffer.g4;
+		var g2 = framebuffer.g2;
 		g2.begin();
-		g2.setPipeline(pipeline);
+		y += (moveY);
+		x += (moveX);
+		
+		if(mdown) {
+			moveY = 0;
+			moveX = 0;
+		}else{
+			moveX *= friction;
+			moveY *= friction;
+		}
+		
+		//g2.setPipeline(pipeline);
 		g2.clear(kha.Color.fromFloats(0.2, 0.4, 0.7));
+		g2.drawLine(0, 0, 20 + x, 20 + y, 3);
 		g2.end();
 		//g2.begin(true, kha.Color.fromFloats(0.2, 0.4, 0.6));
 		//g2.end();
